e060ec165bb0357a116921a104be88765d7c96ba
wmoecke <wmoecke@users.noreply.github.com>
2015-10-09 16:13:03 -0300

    Updates, fixes
    Fixed a couple modes (CHASER, POLICE LIGHTS CHASER)
    Added new modes (ZONE CHASER, AUDIO SPECTRUM)

--- Firmware/SparkPixels.ino
+++ Firmware/SparkPixels.ino
@@ -45,11 +45,13 @@
  
 #include "neopixel/neopixel.h"
 #include <math.h>
+#define ON                      1
+#define OFF                     0
 
 //NEOPIXEL Defines
-#define PIXEL_CNT 512
-#define PIXEL_PIN D0
-#define PIXEL_TYPE WS2812B
+#define PIXEL_CNT               512
+#define PIXEL_PIN               D0
+#define PIXEL_TYPE              WS2812B
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_CNT, PIXEL_PIN, PIXEL_TYPE);
 
@@ -78,22 +80,42 @@ const int POLICELIGHTSWIPE            = 19;
 const int POLICELIGHTSONE             = 20;
 const int LISTENER                    = 21;
 const int ZONECHASER                  = 22;
+const int SPECTRUM                    = 23;
 
-typedef struct modeParams
-{
+typedef struct modeParams {
    int 	 modeId;
    char  modeName[64];
    int   numOfColors;       //Tell the android app home many colors to expect. Max number is 6
 } modeParams;
 
 /**   An RGB color. */
-typedef struct Color
-{
+typedef struct Color {
   unsigned char red, green, blue;
 
   Color(int r, int g, int b) : red(r), green(g), blue(b) {}
   Color() : red(0), green(0), blue(0) {}
 } Color;
+
+/** Overloaded != operator. */
+bool operator!= (const Color& a, const Color& b);
+
+/** Overloaded == operator. */
+bool operator== (const Color& a, const Color& b);
+
+/** Common colors */
+const Color black     = Color(0x00, 0x00, 0x00);
+const Color grey      = Color(0x92, 0x95, 0x91);
+const Color yellow    = Color(0xff, 0xff, 0x14);
+const Color magenta   = Color(0xc2, 0x00, 0x78);
+const Color orange    = Color(0xf9, 0x73, 0x06);
+const Color teal      = Color(0x02, 0x93, 0x86);
+const Color red       = Color(0xe5, 0x00, 0x00);
+const Color brown     = Color(0x65, 0x37, 0x00);
+const Color pink      = Color(0xff, 0x81, 0xc0);
+const Color blue      = Color(0x03, 0x43, 0xdf);
+const Color green     = Color(0x15, 0xb0, 0x1a);
+const Color purple    = Color(0x7e, 0x1e, 0x9c);
+const Color white     = Color(0xff, 0xff, 0xff);
 
 /* ======================= ADD NEW MODE STRUCT HERE. ======================= */
 //modeId and modeName should be the same name to prevent confusion
@@ -124,13 +146,14 @@ modeParams modeStruct[] =
         {  POLICELIGHTSWIPE,            "POLICE LIGHTS WIPE",   0   },
         {  RAINBOW,                     "RAINBOW",              0   },
         {  RAINBOW_BURST,               "RAINBOW BURST",        0   },
+        {  SPECTRUM,                    "AUDIO SPECTRUM",	    0   },
         {  THEATERCHASE,                "THEATER CHASE",	    0   },
         {  ZONE,                        "ZONE",				    4   },
         {  ZONECHASER,                  "ZONE CHASER",			4   }
 };
 
 //Preset speed constants
-const int speedPresets[] = { 120, 100, 80, 70, 50, 30, 20, 10, 1};  //in mSec, slow to fast       
+const int speedPresets[] = {120, 100, 80, 70, 50, 30, 20, 10, 1};  //in mSec, slow to fast       
 
 //Temp Sensor constants	
 //double refVoltage = 3.3;    //Voltage used as reference for analogRead
@@ -197,7 +220,7 @@ int zone4End   = PIXEL_CNT;             //512
 int randomFlakes[(int)(PIXEL_CNT*0.1)]; // holds the snowflake positions no more than10% of total number of pixels
 Color snowFlakeColor;
 
-//LISTENER mode defines
+/* ========================== LISTENER mode defines ========================== */
 UDP Udp;    //an UDP instance to let us receive packets over UDP
 //some tpm2.net constants
 #define TPM2NET_LISTENING_PORT  65506
@@ -213,6 +236,22 @@ UDP Udp;    //an UDP instance to let us receive packets over UDP
 #define BPP                     3       // 3 bytes per pixel or 24bit (RGB)
 // Package size we expect. The footer byte is not included here!
 #define EXPECTED_PACKED_SIZE    (PIXELS_PER_PANEL * BPP + TPM2NET_HEADER_SIZE)
+
+/* ======================= AUDIO SPECTRUM mode defines ======================= */
+#define MICROPHONE              12
+#define GAIN_CONTROL            11
+#define SIDE                    NR_OF_PANELS // 8
+#define TRAIL_LENGTH            50
+#define SAMPLES                 2048
+#define M                       4   // If the M value changes, then the 'ARRAY_SIZE' constant also needs
+#define ARRAY_SIZE              16  // to be changed to reflect the result of the formula: pow(2,M)
+#define INPUTLEVEL              63  // This sets the sensitivity for the onboard AGC circuit (0-255); the higher, the more sensitive
+#define SMOOTHLEDS              ON  // Switch for 'smoothening-out' the LEDs fade-to-black on the spectrum
+#define DOTMODE                 ON  // Switch to control how the peaks will be displayed over each strip (bar/dot)
+float real[ARRAY_SIZE];             //[(int)pow(2,M)]
+float imaginary[ARRAY_SIZE];        //[(int)pow(2,M)]
+float maxVal=8;
+
 /* ======================= Required Prototypes =============================== */
 int showPixels(void);
 int antipodal_index(int i);
@@ -221,18 +260,22 @@ int getModeIndexFromID(int id);
 int isValidMode(String newMode);
 int getModeIndexFromName(String name);
 //int getTemperature(void);
+void initMicrophone(void);
 void fadeInToColor(uint32_t index, Color col);
 void fadeOutFromColor(uint32_t index, Color col);
+short FFT(short int dir,int m,float *x,float *y);
 uint32_t Wheel(byte WheelPos);
 Color getPixelColor(uint32_t index);
 Color getColorFromInteger(uint32_t col);
 uint32_t getHighestValFromRGB(Color col);
+uint32_t colorMap(float val, float minVal, float maxVal);
 uint32_t lerpColor(uint32_t c1, uint32_t c2, uint32_t val, uint32_t minVal, uint32_t maxVal);
 
 /* ======================= Spark Pixel Prototypes =============================== */
 int colorAll(uint32_t c);
 int colorWipe(uint32_t c);
 int colorZone(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4);
+void FFTJoy(void);
 void frozen(void);
 void listen(void);
 void collide(void);
@@ -255,10 +298,10 @@ void theaterChaseRainbow(void);
 void findRandomSnowFlakesPositions(int numFlakes);
 void colorZoneChaser(uint32_t c1, uint32_t c2, uint32_t c3, uint32_t c4);
 
-/************** DISABLED AS IT INTERFERES WITH UDP LISTENING **************/
-//Don't connect to the cloud first so we can turn on the lights right away
-//SYSTEM_MODE(SEMI_AUTOMATIC);
-/**************************************************************************/
+/* ============ DISABLED AS IT INTERFERES WITH UDP LISTENING ============
+ * Don't connect to the cloud first so we can turn on the lights right away
+ * SYSTEM_MODE(SEMI_AUTOMATIC);
+ * ====================================================================== */
 
 void setup() {
     Particle.function("SetMode", SetMode);
@@ -317,7 +360,10 @@ void setup() {
     
     // Start the UDP
     if(Udp.setBuffer(EXPECTED_PACKED_SIZE))
-        Udp.begin(TPM2NET_LISTENING_PORT);    
+        Udp.begin(TPM2NET_LISTENING_PORT);
+    
+    // Initialize audio capture
+    initMicrophone();
 }
 
 //delay (or speed) is passed 
@@ -362,6 +408,9 @@ void loop() {
     		    break;    	
     		case RAINBOW:
     		    rainbowCycle();
+    		    break;
+    		case SPECTRUM:
+    		    FFTJoy();
     		    break;
     		case THEATERCHASE:
     		    theaterChaseRainbow();
@@ -1246,7 +1295,61 @@ uint32_t Wheel(byte WheelPos) {
 
   @return Color between colors a and b.*/
 uint32_t lerpColor(uint32_t c1, uint32_t c2, uint32_t val, uint32_t minVal, uint32_t maxVal) {
-    return c1 + (c2-c1) * (val-minVal) / (maxVal-minVal);
+    Color a = getColorFromInteger(c1);
+    Color b = getColorFromInteger(c2);
+    int red = a.red + (b.red-a.red) * (val-minVal) / (maxVal-minVal);
+    int green = a.green + (b.green-a.green) * (val-minVal) / (maxVal-minVal);
+    int blue = a.blue + (b.blue-a.blue) * (val-minVal) / (maxVal-minVal);
+    
+    return strip.Color(red, green, blue);
+}
+
+/** Returns a color from a set of colors fading from blue to green to red and back again
+    the color is returned based on where the parameter *val* falls between the parameters
+    *min* and *max*.  If *val* is min, the function returns a blue color.  If *val* is halfway
+    between *min* and *max*, the function returns a yellow color.*/  
+uint32_t colorMap(float val, float minVal, float maxVal) {
+    const float range = 2048;
+    val = range * (val-minVal) / (maxVal-minVal);
+    
+    Color colors[6];
+    
+    colors[0].red = 0;
+    colors[0].green = 0;
+    colors[0].blue = brightness;
+    
+    colors[1].red = 0;
+    colors[1].green = brightness;
+    colors[1].blue = brightness;
+    
+    colors[2].red = 0;
+    colors[2].green = brightness;
+    colors[2].blue = 0;
+    
+    colors[3].red = brightness;
+    colors[3].green = brightness;
+    colors[3].blue = 0;
+    
+    colors[4].red = brightness;
+    colors[4].green = 0;
+    colors[4].blue = 0;
+    
+    colors[5].red = brightness;
+    colors[5].green = 0;
+    colors[5].blue = brightness;
+    
+    if(val <= range/6)
+        return lerpColor(strip.Color(colors[0].red, colors[0].green, colors[0].blue), strip.Color(colors[1].red, colors[1].green, colors[1].blue), val, 0, range/6);
+    else if(val <= 2 * range / 6)
+        return(lerpColor(strip.Color(colors[1].red, colors[1].green, colors[1].blue), strip.Color(colors[2].red, colors[2].green, colors[2].blue), val, range / 6, 2 * range / 6));
+    else if(val <= 3 * range / 6)
+        return(lerpColor(strip.Color(colors[2].red, colors[2].green, colors[2].blue), strip.Color(colors[3].red, colors[3].green, colors[3].blue), val, 2 * range / 6, 3*range / 6));
+    else if(val <= 4 * range / 6)
+        return(lerpColor(strip.Color(colors[3].red, colors[3].green, colors[3].blue), strip.Color(colors[4].red, colors[4].green, colors[4].blue), val, 3 * range / 6, 4*range / 6));
+    else if(val <= 5 * range / 6)
+        return(lerpColor(strip.Color(colors[4].red, colors[4].green, colors[4].blue), strip.Color(colors[5].red, colors[5].green, colors[5].blue), val, 4 * range / 6, 5*range / 6));
+    else
+        return(lerpColor(strip.Color(colors[5].red, colors[5].green, colors[5].blue), strip.Color(colors[0].red, colors[0].green, colors[0].blue), val, 5 * range / 6, range));
 }
 
 /** Find index of antipodal opposite LED **/
@@ -1514,7 +1617,7 @@ void listen() {
                     for(int col = 0; col < NR_OF_PANELS; col++) {           // Linewise, starting from leftmost index
                         for(int row = NR_OF_PANELS - 1; row >= 0; row--) {  // Columnwise, starting from topmost index
                             Color pixelColor = Color(data[voxelIdx], data[voxelIdx + 1], data[voxelIdx + 2]);  // Take 3 color bytes from buffer
-                            int idx = (row*64) + (z*8) + col;
+                            int idx = (row*NR_OF_PANELS*NR_OF_PANELS) + (z*NR_OF_PANELS) + col;
                             strip.setPixelColor(idx, strip.Color(pixelColor.red, pixelColor.green, pixelColor.blue));
                         }
                         voxelIdx+=3; // Increment buffer index by 3 bytes
@@ -1533,4 +1636,164 @@ void listen() {
             }
         }
     }
+}
+
+/* ============ AUDIO SPECTRUM mode functions ============ */
+void FFTJoy() {
+    run = TRUE;
+    
+    for(int i=0;i<pow(2,M);i++) {
+        real[i]=analogRead(MICROPHONE)-SAMPLES;
+        delayMicroseconds(120);  /* 210 this sets our 'sample rate'.  I went through a bunch of trial and error to 
+                                  * find a good sample rate to put a soprano's vocal range in the spectrum of the cube
+    							  *
+      							  * 120 gets the entire range from bass to soprano within the cube's spectrum
+      							  * *From bass to soprano: https://youtu.be/6jqCuE7C3rg */
+        imaginary[i]=0;
+        if(stop == TRUE) {return;}
+    }
+    
+    FFT(1, M, real, imaginary);
+    
+    for(int i=0;i<pow(2,M);i++) {
+        imaginary[i]=sqrt(pow(imaginary[i],2)+pow(real[i],2));
+        if(imaginary[i]>maxVal)
+            maxVal=imaginary[i];
+        if(stop == TRUE) {return;}
+    }
+
+    for(int i=0;i<pow(2,M)/2;i++) {
+        imaginary[i]=SIDE*imaginary[i]/maxVal;
+        Color pixelColor;
+        int y;
+    	for(y=0;y<=imaginary[i];y++) {
+          	pixelColor=getColorFromInteger(colorMap(y,0,SIDE));
+            strip.setPixelColor(((SIDE-1)*SIDE*SIDE) + (i*SIDE) + y, strip.Color(pixelColor.red, pixelColor.green, pixelColor.blue));
+    		if(DOTMODE) {
+                pixelColor=black;
+                if(SMOOTHLEDS) {
+                    pixelColor=getPixelColor(((SIDE-1)*SIDE*SIDE) + (i*SIDE) + (y>0 ? y-1 : 1));
+                    //this creates a fading 'trail' across the y-axis from the base of the
+                  	//cube towards the peak; gotta fade the pixels *much* faster, so that 
+                  	//the human eye can spot the 'trailing' effect in 'dot' mode
+                    if(pixelColor.red > 0) pixelColor.red-=pixelColor.red*.25;
+                    if(pixelColor.green > 0) pixelColor.green-=pixelColor.green*.25;
+                    if(pixelColor.blue > 0) pixelColor.blue-=pixelColor.blue*.25;
+                }
+    			strip.setPixelColor(((SIDE-1)*SIDE*SIDE) + (i*SIDE) + (y>0 ? y-1 : 1), strip.Color(pixelColor.red, pixelColor.green, pixelColor.blue));
+    			delayMicroseconds(speed);  //introducing a little bit of delay to 'smoothen-out' transitions
+            }
+            if(stop == TRUE) {return;}
+    	}
+    	
+    	for(;y<SIDE;y++) {
+            pixelColor=black;
+            if(SMOOTHLEDS) {
+                pixelColor=getPixelColor(((SIDE-1)*SIDE*SIDE) + (i*SIDE) + y);
+              	//fade the pixels as the audio level drops; causes a nice and smooth 
+              	//'trailing' effect from the top of the cube (y-axis) towards the peak
+                if(pixelColor.red > 0) pixelColor.red-=pixelColor.red*.25;
+                if(pixelColor.green > 0) pixelColor.green-=pixelColor.green*.25;
+                if(pixelColor.blue > 0) pixelColor.blue-=pixelColor.blue*.25;
+            }
+            strip.setPixelColor(((SIDE-1)*SIDE*SIDE) + (i*SIDE) + y, strip.Color(pixelColor.red, pixelColor.green, pixelColor.blue));
+            delayMicroseconds(speed);  //introducing a little bit of delay to 'smoothen-out' transitions
+            if(stop == TRUE) {return;}
+    	}
+    }
+    
+    for(int z=0;z<SIDE-1;z++)
+        for(int x=0;x<SIDE;x++)
+            for(int y=0;y<SIDE;y++) {
+                Color trailColor=getPixelColor(((z+1)*SIDE*SIDE) + (x*SIDE) + y);
+                if(SMOOTHLEDS) {
+                    //fade the trail to black over the length of the cube's z-axis
+                  	//this is responsible for the 'meteors' shooting towards the back of 
+                  	//the cube; otherwise it would look like they were 'going backwards'
+                    if(trailColor.red > 0) trailColor.red-=trailColor.red*.125;
+                    if(trailColor.green > 0) trailColor.green-=trailColor.green*.125;
+                    if(trailColor.blue > 0) trailColor.blue-=trailColor.blue*.125;
+                }
+                strip.setPixelColor((z*SIDE*SIDE) + (x*SIDE) + y, strip.Color(trailColor.red, trailColor.green, trailColor.blue));
+                delayMicroseconds(speed);  //introducing a little bit of delay to 'smoothen-out' transitions
+                if(stop == TRUE) {return;}
+            }
+    
+    maxVal= (maxVal>=120) ? maxVal-2 : (maxVal<8) ? 8 : maxVal-.8;
+    showPixels();
+    if(stop == TRUE) {return;}
+}
+
+void initMicrophone() {
+  pinMode(GAIN_CONTROL, OUTPUT);
+  analogWrite(GAIN_CONTROL, INPUTLEVEL);  //digitalWrite(GAIN_CONTROL, LOW);
+}
+
+short FFT(short int dir,int m,float *x,float *y) {
+   int n,i,i1,j,k,i2,l,l1,l2;
+   float c1,c2,tx,ty,t1,t2,u1,u2,z;
+
+   /* Calculate the number of points */
+   n = 1;
+   for (i=0;i<m;i++) 
+      n *= 2;
+
+   /* Do the bit reversal */
+   i2 = n >> 1;
+   j = 0;
+   for (i=0;i<n-1;i++) {
+      if (i < j) {
+         tx = x[i];
+         ty = y[i];
+         x[i] = x[j];
+         y[i] = y[j];
+         x[j] = tx;
+         y[j] = ty;
+      }
+      k = i2;
+      while (k <= j) {
+         j -= k;
+         k >>= 1;
+      }
+      j += k;
+   }
+
+   /* Compute the FFT */
+   c1 = -1.0; 
+   c2 = 0.0;
+   l2 = 1;
+   for (l=0;l<m;l++) {
+      l1 = l2;
+      l2 <<= 1;
+      u1 = 1.0; 
+      u2 = 0.0;
+      for (j=0;j<l1;j++) {
+         for (i=j;i<n;i+=l2) {
+            i1 = i + l1;
+            t1 = u1 * x[i1] - u2 * y[i1];
+            t2 = u1 * y[i1] + u2 * x[i1];
+            x[i1] = x[i] - t1; 
+            y[i1] = y[i] - t2;
+            x[i] += t1;
+            y[i] += t2;
+         }
+         z =  u1 * c1 - u2 * c2;
+         u2 = u1 * c2 + u2 * c1;
+         u1 = z;
+      }
+      c2 = sqrt((1.0 - c1) / 2.0);
+      if (dir == 1) 
+         c2 = -c2;
+      c1 = sqrt((1.0 + c1) / 2.0);
+   }
+
+   /* Scaling for forward transform */
+   if (dir == 1) {
+      for (i=0;i<n;i++) {
+         x[i] /= n;
+         y[i] /= n;
+      }
+   }
+
+   return(0);
 }
